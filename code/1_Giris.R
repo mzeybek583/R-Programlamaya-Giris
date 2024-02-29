3bc36b10e848004608593747976b833af978812a
mzeybek <muzo583@gmail.com>
2024-02-29 11:28:39 +0300

    Create 1_Giris.R

---
+++ code/1_Giris.R
@@ -0,0 +1,24 @@
+
+### Değişkenler ve Atama İşlemleri
+
+x <- 10
+y <- "Merhaba, dünya!"
+
+### Vektörler
+
+sayilar <- c(1, 2, 3, 4, 5)
+meyveler <- c("elma", "armut", "kiraz")
+
+### Fonksiyonlar
+
+topla <- function(a, b) {
+  return(a + b)
+}
+sonuc <- topla(3, 5)
+
+### Veri Çerçeveleri (Data Frames)
+
+ogrenciler <- data.frame(
+  Ad = c("Ali", "Ayşe", "Mehmet"),
+  Yas = c(20, 22, 21)
+)
